0d891fae26f00d7c3bb9dcd102842eadd64b568a
Razin <iamrazin47@gmail.com>
2025-02-06 23:21:10 +0530

    Frontent Hero Design

--- client/src/Components/Home/Hero.jsx
+++ client/src/Components/Home/Hero.jsx
@@ -1,28 +1,36 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Image, Row } from "react-bootstrap";
 import './Home.css'
 const Hero = () => {
   return (
-    <Container  fluid className="hero-container">
-        <div className="gradient-wrap">
-          <Row>
-            <Col md={3}>
-                <div className="hot-tag">
-                  <p>#1 Best medical Center</p>
+    <Container  fluid className="hero-container d-flex">
+        <div className="hero-container-2  ">
+          <Row className="hero-container-2-row">
+            <Col md={5} className="hero-content-text d-flex ">
+                
+                <div className="hero-title  ">
+
+                  <div className="hot-tag">
+                  <p className="hero-text-hot-tag">#1 Best medical Center</p>
+                  </div>
+
+                  <p className="hero-text-main">
+                    The <span className="main-text-highlight">Best Medical </span>  and Treatment  Center For You
+                  </p>
+                  <p className="hero-text-bottom">Connect with our professional doctors who are ready to help you manage <br /> your help you manage your health with expertise and dedication</p>
+
+                  <div className="hero-book-appointment">
+                    <span>                      Book An Appointment
+                    </span>
+                  </div>
                 </div>
 
-                <div>
-                  <h1>
-                    The Best Medical Center and Treatment Center For You
-                  </h1>
-                </div>
-
-                <div>
-                  <p>Connect with our professional doctors who are ready to help you manage your help you manage your health with expertise and dedication</p>
-                </div>
-                <div>
-                  
-                </div>
+                
+            </Col>
+            <Col className=""> 
+              <div className="w-100 ">
+                <Image className="w-100" src="/media/very-good-smiling-confident-asian-female-doctor-showing-okay-ok-sign-approval-confirm-smth-saying.png" alt="asian-hero-doctor" />
+              </div>
             </Col>
           </Row>
         </div>
